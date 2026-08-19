8d0c12cfc9fb5f7c48dee0ab303d65d15c47d309
Rohan_Kanade <kanaderohan83@gmail.com>
2026-08-19 18:15:08 +0530

    TrustSection components created

---
+++ src/components/TrustSection.jsx
@@ -0,0 +1,41 @@
+function TrustSection() {
+  return (
+    <section className="trust-section">
+
+      <div className="container">
+
+        <p>
+          YOUR PERSONAL AI COMPANION FOR
+        </p>
+
+        <div className="trust-items">
+
+          <span>
+            <i className="bi bi-code-square"></i>
+            Coding
+          </span>
+
+          <span>
+            <i className="bi bi-mortarboard"></i>
+            Learning
+          </span>
+
+          <span>
+            <i className="bi bi-person-workspace"></i>
+            Interviews
+          </span>
+
+          <span>
+            <i className="bi bi-rocket-takeoff"></i>
+            Career
+          </span>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default TrustSection;
